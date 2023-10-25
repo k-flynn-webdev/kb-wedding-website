92cbf360c756e28aea0207b9e210718184c8e3d0
kevinflynn <flynny85@gmail.com>
2023-10-25 15:21:59 +0100

    rsvp page looking good

--- nuxt/pages/rsvp.vue
+++ nuxt/pages/rsvp.vue
@@ -2,6 +2,9 @@
 definePageMeta({
   // middleware: ["protected"],
 });
+
+const stayingNight = ref(false);
+const highChair = ref(false);
 </script>
 
 <template>
@@ -28,15 +31,83 @@ definePageMeta({
         class="input input-bordered input-primary w-full"
       />
 
-      <div class="form-control w-52">
-        <label class="cursor-pointer label">
-          <span class="label-text">Remember me</span>
-          <input
-            type="checkbox"
-            class="toggle toggle-primary"
-            checked
-          />
-        </label>
+      <div class="form-control">
+        <div class="section">
+          <div
+            class="tooltip"
+            data-tip="Meal starts at 3:30PM, 7PM side buffet"
+          >
+            <p>Wedding Meal</p>
+          </div>
+
+          <label class="cursor-pointer label">
+            <span class="">Chicken</span>
+            <input
+              type="checkbox"
+              class="toggle toggle-success"
+            />
+          </label>
+
+          <label class="cursor-pointer label">
+            <span class="">Lamb</span>
+            <input
+              type="checkbox"
+              class="toggle toggle-success"
+            />
+          </label>
+
+          <label class="cursor-pointer label">
+            <span class="">Vegetarian</span>
+            <input
+              type="checkbox"
+              class="toggle toggle-success"
+            />
+          </label>
+
+          <div
+            class="tooltip mt-5"
+            data-tip="A half adult size meal"
+          >
+            <label class="cursor-pointer label">
+              <span class="">Kids Meal</span>
+              <input
+                type="checkbox"
+                class="toggle toggle-success"
+              />
+            </label>
+          </div>
+
+          <div
+            class="tooltip"
+            data-tip="The venue has 7 high chairs available"
+          >
+            <label class="cursor-pointer label">
+              <span class="">Require High Chair</span>
+              <input
+                type="checkbox"
+                class="toggle toggle-success"
+                v-model="highChair"
+              />
+            </label>
+          </div>
+        </div>
+
+        <div class="section">
+          <p>Accomodation</p>
+          <div
+            class="tooltip"
+            data-tip="The venue has 33 rooms available"
+          >
+            <label class="cursor-pointer label">
+              <span class="">Staying At Ravenswood</span>
+              <input
+                type="checkbox"
+                class="toggle toggle-success"
+                v-model="stayingNight"
+              />
+            </label>
+          </div>
+        </div>
       </div>
     </div>
   </div>
