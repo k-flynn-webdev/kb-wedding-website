599df967212ee7e9652b484421196998a728f296
kevinflynn <flynny85@gmail.com>
2023-10-27 19:26:47 +0100

    cleanup simplify

--- nuxt/components/footer.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-
-<template>
-  <!-- <div class="footer">
-    <div class="flex-1">
-      <p class="text-center">Footer</p>
-    </div>
-  </div> -->
-</template>
--- nuxt/interfaces/index.ts
+++ nuxt/interfaces/index.ts
@@ -3,23 +3,3 @@ interface defaultCreateUpdateDelete {
   updated_at: string;
   deleted_at: string;
 }
-
-export interface colorHSL {
-  h: number;
-  s: number;
-  l: number;
-}
-
-export interface User {
-  userId: string;
-  email: string;
-  emailVerified: boolean;
-}
-
-export interface DateItem extends defaultCreateUpdateDelete {
-  id: string;
-  user_id: string;
-  event_id: string;
-  place_id: string;
-  date_time: string;
-}
