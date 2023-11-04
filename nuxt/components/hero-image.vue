dc8ca4a98c8fd3dfb034e659a8cf2bfb687752f7
kevinflynn <flynny85@gmail.com>
2023-11-04 18:36:55 +0000

    fix image reload

--- nuxt/components/hero-image.vue
+++ nuxt/components/hero-image.vue
@@ -3,14 +3,19 @@ import { useRoute } from "#vue-router";
 import { randomIntFromInterval } from "@/helpers/math";
 
 const route = useRoute();
-
 const randomNum = randomIntFromInterval(1, 17);
+
 const randomImageName = `image-${randomNum < 10 ? "0" : ""}${randomNum}`;
 </script>
 
 <template>
   <div
     class="hero-image"
-    :class="[randomImageName, { dark: route?.name !== 'index' }]"
-  ></div>
+    :class="[{ dark: route?.name !== 'index' }]"
+  >
+    <div
+      class="hero-image-content"
+      :class="randomImageName"
+    ></div>
+  </div>
 </template>
