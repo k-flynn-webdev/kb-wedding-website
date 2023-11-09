d75290b2a1f61a12426c9b4ec6da24ec05d96d91
kevinflynn <flynny85@gmail.com>
2023-11-09 23:29:03 +0000

    making config init less verbose

--- nuxt/server/api/init/server-init.ts
+++ nuxt/server/api/init/server-init.ts
@@ -1,4 +1,9 @@
 const config = useRuntimeConfig();
 
-console.log(":: Server config ::");
-console.log(config);
+console.log(":: Server config START ::");
+console.log("   - APP");
+console.log(config.app);
+console.log("   - NITRO");
+console.log(config.nitro);
+console.log(":: Server config DONE ::");
+console.log("\n");
