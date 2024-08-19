78275579042148805bc45980b066608aff06200c
kevinflynn <flynny85@gmail.com>
2024-08-19 22:08:44 +0100

    add photos page

---
+++ nuxt/pages/photos.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="wedding-hero">
+    <div class="wedding-hero__heading extra">
+      <div class="transform">
+        <h1>Photos</h1>
+      </div>
+    </div>
+  </div>
+
+  <div class="content">
+    <ul>
+      <NuxtLink
+        to="https://photos.app.goo.gl/ViGee8sYC8vhJxkK7"
+        class="link"
+        >Social Album
+      </NuxtLink>
+
+      <br />
+      <br />
+      <br />
+
+      <NuxtLink
+        to="https://photos.app.goo.gl/wdAfwaZ3GoouXCDe6"
+        class="link"
+        >Photographer Album
+      </NuxtLink>
+
+      <br />
+      <br />
+      <br />
+
+      <NuxtLink
+        to="https://photos.app.goo.gl/7UES5NAXbLo1HLA8A"
+        class="link"
+        >Photobooth Album
+      </NuxtLink>
+    </ul>
+  </div>
+</template>
